2da462d6fb950201b32b6dc9484e272018af4530
monupal1122 <monupalji1122@gmail.com>
2026-02-12 13:00:25 +0530

    Remove nanoid dependency and use built-in crypto

--- src/utils/idGenerator.js
+++ src/utils/idGenerator.js
@@ -1,12 +1,18 @@
-const { customAlphabet } = require('nanoid');
+const crypto = require('crypto');
 
 /**
- * Generates a unique numeric-only ID using nanoid with digits 0-9.
+ * Generates a unique numeric-only ID using built-in crypto with digits 0-9.
  * The ID is 12 characters long for high uniqueness (10^12 possibilities).
  * This function generates the ID but does not check for database uniqueness.
  * Uniqueness check is handled in the model pre-save hook.
  */
-const generateNumericId = customAlphabet('0123456789', 12);
+const generateNumericId = (length = 12) => {
+    let result = '';
+    while (result.length < length) {
+        result += crypto.randomInt(0, 10).toString();
+    }
+    return result;
+};
 
 module.exports = {
     generateNumericId
