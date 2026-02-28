5a215d9280e4b37e644c11e0527fcf5d1be860eb
monupal1122 <monupalji1122@gmail.com>
2026-02-28 17:21:55 +0530

    update

--- server.js
+++ server.js
@@ -47,18 +47,7 @@ app.use((req, res, next) => {
     next();
 });
 
-const cors = require("cors");
-
-app.use(
-  cors({
-    origin: [
-      "https://korsimnaturals.com",
-      "https://www.korsimnaturals.com",
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(helmet({
     contentSecurityPolicy: false,
 }));
